bb3f8ae7395ec4fc85957d5e69b50426cfd72757
Aakash Rawat <01tripster@gmail.com>
2022-11-30 07:38:15 +0530

    task 1: make health check route

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+app.get("/healthcheck", (req, res) => {
+	res.send("API working Fine!");
+});
+
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`TODO's app listening on port ${port}`);
 });
