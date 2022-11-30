00c7f9a90836769b84715ed97d174534a7ccd9f3
Aakash Rawat <01tripster@gmail.com>
2022-11-30 12:15:04 +0530

    task 3.3: get request to fetch all todo items

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const nanoid = require("nanoid");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const db = "todoapp";
+const collection = "todo_items";
+
 app.get("/healthcheck", (req, res) => {
 	res.status(200).json({
 		success: true,
@@ -16,7 +19,7 @@ app.get("/healthcheck", (req, res) => {
 	});
 });
 
-app.post("/addtodo", async (req, res) => {
+app.post("/api/v1/addtodo", async (req, res) => {
 	console.log(req.body);
 	const { email, task } = req.body;
 	const date = Date.now();
@@ -29,10 +32,7 @@ app.post("/addtodo", async (req, res) => {
 		task,
 	};
 	try {
-		const result = await client
-			.db("todoapp")
-			.collection("todo_items")
-			.insertOne(data);
+		const result = await client.db(db).collection(collection).insertOne(data);
 		console.log(`New task created with id: ${result.insertedId}`);
 
 		res.status(200).json({
@@ -49,12 +49,9 @@ app.post("/addtodo", async (req, res) => {
 	}
 });
 
-app.get("/todo/:id", async (req, res) => {
+app.get("/api/v1/todo/:id", async (req, res) => {
 	const id = req.query;
-	const result = await client
-		.db("todoapp")
-		.collection("todo_items")
-		.findOne(id);
+	const result = await client.db(db).collection(collection).findOne(id);
 
 	if (result) {
 		res.status(200).json({
@@ -62,10 +59,75 @@ app.get("/todo/:id", async (req, res) => {
 			task: result.task,
 			date: result.data,
 		});
+		await client.close();
 	} else {
 		res.status(400).json({
 			success: false,
 			message: "something went wrong!!",
+		});
+	}
+});
+
+app.get("/api/v1/alltasks", async (req, res) => {
+	const { email } = req.body;
+	try {
+		const query = { email };
+		const result = client.db(db).collection(collection).find(query);
+		res.status(200).json({
+			success: true,
+			result,
+		});
+		await client.close();
+	} catch {
+		res.status(400).json({
+			success: false,
+			message: `something went wrong while fetching all the todo asks of the user with email: ${email}`,
+		});
+	}
+});
+
+app.patch("/api/v1/updateonetodo", async (req, res) => {
+	try {
+		const { email, _id, newtask } = req.body;
+		const updatedtask = {
+			task: `${newtask}`,
+		};
+
+		await client
+			.db(db)
+			.collection(collection)
+			.updateOne({ email, _id }, { $set: updatedtask });
+
+		res.status(200).json({
+			success: true,
+			message: `todo item updated!`,
+		});
+
+		await client.close();
+	} catch {
+		res.status(400).json({
+			success: false,
+			message: "something went wrong!",
+		});
+	}
+});
+
+app.delete("/api/v1/deleteonetodo", async (req, res) => {
+	try {
+		const { _id } = req.body;
+
+		await client.db(db).collection(collection).deleteOne({ _id });
+
+		res.status(200).json({
+			success: true,
+			message: `todo item deleted!`,
+		});
+
+		await client.close();
+	} catch {
+		res.status(400).json({
+			success: false,
+			message: "something went wrong while deleting the todo item!",
 		});
 	}
 });
